d338a93fdf166cef547c156c670887bf19192325
jan <janluca0000046@gmail.com>
2025-11-30 23:31:15 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import express from 'express'
-import { readdirSync } from 'node:fs'
+import { readdirSync, statSync } from 'node:fs'
 import { processFile } from './pipeline.js'
 
 
 const app = express()
 
+const GIBIBYTE = 1024 ** 3;
+const MAX_SIZE = 2 * GIBIBYTE;
+
 app.get('/', async (req, res) => {
     const dir = readdirSync('/mnt/Movies/backup/'+req.query.path)
     console.log(dir)
     dir.forEach(async file => {
+        const stats = statSync(`/mnt/Movies/backup/${req.query.path}/${file}`)
+        if(stats.size < MAX_SIZE) return;
         await processFile(`/mnt/Movies/backup/${req.query.path}/${file}`, req.query.show)
     })
     res.send('done')
