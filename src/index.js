914acedaf64088a08e83f4183c3d512ef5e96b48
jan <janluca0000046@gmail.com>
2025-11-30 17:25:12 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { processFile } from './pipeline.js'
 const app = express()
 
 app.get('/', (req, res) => {
-    processFile('/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/00000.m2ts', 'That 70s Show')
+    processFile('/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/00003.m2ts', 'That 70s Show')
     res.send('done')
 })
 
