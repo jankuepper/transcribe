80692c7999e643ac90ade564f98db829fb1dc05a
jan <janluca0000046@gmail.com>
2025-11-30 23:16:15 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import { readdirSync } from 'node:fs'
 import { processFile } from './pipeline.js'
 
 
 const app = express()
 
 app.get('/', async (req, res) => {
-    await processFile('/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/00000.m2ts', 'that70sshow')
+    console.log(req.params)
+    readdirSync('/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/')
+    // await processFile('/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/00000.m2ts', 'that70sshow')
+    
     res.send('done')
 })
 
