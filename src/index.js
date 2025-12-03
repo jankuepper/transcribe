7e74644ea4787804d411a7c5a0c885c1845380f0
Jan <janluca0000046@gmail.com>
2025-12-03 14:17:13 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -27,12 +27,12 @@ function prep(file, path) {
 }
 
 app.get('/test', (req, res) => {
-  const path = '/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/'
+  const path = '/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D2/BDMV/STREAM/'
   const dir = readdirSync(path)
   dir.forEach(file => {
     if (file.split('.')[1] !== 'm2ts' || statSync(path + file).size < MAX_SIZE) return;
     const name = file.split('.')[0]
-    command('ffmpeg', ['-i', path + name + '.m2ts', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a', '192k', '/mnt/media/shows/that70sshow/season_2/' + Number(name) + '_raw' + '.mp4'])
+    command('ffmpeg', ['-i', path + name + '.m2ts', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a', '192k', '/mnt/media/shows/that70sshow/season_2/' + (12 + Number(name)) + '_raw' + '.mp4'])
     //    command('cp', [name + '.mp4', '/mnt/media/shows/that70sshow/season_2/'])
   })
   res.send('done')
