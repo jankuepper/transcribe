77971d264119c0331f78e7bc8fdcebcbff4ac52b
Jan <janluca0000046@gmail.com>
2025-12-03 12:39:07 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function prep(file, path) {
 app.get('/test', (req, res) => {
   const path = '/mnt/Movies/backup/brseason2/THAT70SSHOW_S2D1/BDMV/STREAM/'
   command('rm', ['*.m2ts'])
-  command('cp', [path, '.'])
+  command('cp', ['-r', path, '.'])
   const localDir = readdirSync('.')
   localDir.forEach(file => {
     if (statSync(file).size < MAX_SIZE) return;
