e26409112eca475fc74c461c9b2db77f3d8f1ae2
Jan <janluca0000046@gmail.com>
2025-12-03 13:50:39 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -31,8 +31,8 @@ app.get('/test', (req, res) => {
   const dir = readdirSync(path)
   dir.forEach(file => {
     if (file.split('.')[1] !== 'm2ts' || statSync(path + file).size < MAX_SIZE) return;
-    const name = Number(file.split('.')[0])
-    command('ffmpeg', ['-i', path + name + '.m2ts', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a', '192k', '/mnt/media/shows/that70sshow/season_2/' + name + '.mp4'])
+    const name = file.split('.')[0]
+    command('ffmpeg', ['-i', path + name + '.m2ts', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a', '192k', '/mnt/media/shows/that70sshow/season_2/' + Number(name) + '.mp4'])
     //    command('cp', [name + '.mp4', '/mnt/media/shows/that70sshow/season_2/'])
   })
   res.send('done')
